33fba73fbffa1c81a314d4654810523031bd6342
Paul Fisher <pfisher@alertlogic.com>
2016-06-26 11:49:50 -0500

    Cleanup jslint validation

--- js/erlang.js
+++ js/erlang.js
@@ -1,5 +1,77 @@
+/*jslint es6, node, white */
+"use strict";
 var exec = require('child_process').exec;
 var http = require('http');
+
+function ping(appmod, callback) {
+    const options = {
+        host: 'localhost',
+        port: '7181',
+        path: '/erllambda/' + appmod,
+        method: 'GET'
+    };
+    http.request( options, function(response) {
+        if( response.statusCode === 200 ) { callback(); }
+        else {
+            callback( {error: 'ping response status: '
+                       + response.statusCode} );
+        }
+    }).on('error', function(error) {
+        callback(error);
+    }).end();
+}
+
+
+function alive(appmod, deadline, callback) {
+    const delay = 100;
+    ping( appmod, function(err) {
+        if( !err ) { callback(); }
+        else if( Date.now() < deadline ) {
+            console.log( 'waiting %s ms for erlang start', delay );
+            setTimeout( function() {
+                alive(appmod, deadline, callback);
+            }, delay );
+        }
+        else {
+            callback( { action: 'alive', error: 'timeout waiting for vm' } );
+        }
+    });
+}
+
+
+function stop(script, callback) {
+    const command = script + ' stop';
+    console.log( 'executing: "%s"', command );
+    exec( command, {}, function() {
+        callback();
+    });
+}
+
+function start(appmod, script, env, callback) {
+    const tmp = require('tmp');
+    const glob = require('glob');
+    const command = script + ' start';
+    /* set some additional environment variable needed to drive rebar
+       release scripts. */
+    env.RELX_REPLACE_OS_VARS = "true";
+    env.CORESRV_RUNDIR = tmp.dirSync().name;
+    env.CORESRV_PROGNAME = appmod;
+    env.ERTS_DIR = glob.sync( 'erts-*' )[0];
+    env.HOME = tmp.name;
+    /* no matter what we just issue a stop without looking, to be safe */
+    stop( script, function() {
+        console.log( 'executing: "%s" with env: %s', command,
+                     JSON.stringify(env) );
+        exec( command, {env: env}, function(err) {
+            if( err ) { callback(err); }
+            else {
+                const deadline = Date.now() + 10000;
+                alive(appmod, deadline, callback);
+            }
+        });
+    });
+}
+
 
 module.exports.connect = function(appmod, script, env, callback) {
     /* find an existing Erlang VM running in our container, and if not
@@ -13,12 +85,12 @@ module.exports.connect = function(appmod, script, env, callback) {
        to the serialization guarantees. */
     ping( appmod, function(err) {
         /* if we get a non-error response, we assume it is ready to go. */
-        if( !err ) callback();
+        if( !err ) { callback(); }
         else {
             start( appmod, script, env, callback );
         }
     });
-}
+};
 
 module.exports.invoke = function(appmod, event, context, callback) {
     const body = JSON.stringify( { event: event, context: context } );
@@ -38,7 +110,7 @@ module.exports.invoke = function(appmod, event, context, callback) {
             resp_body += chunk;
         });
         response.on('end', function() {
-            if( response.statusCode == 200 ) {
+            if( response.statusCode === 200 ) {
                 const json = JSON.parse( resp_body );
                 callback( json.error, json.success );
             } else {
@@ -50,73 +122,4 @@ module.exports.invoke = function(appmod, event, context, callback) {
     });
     req.write( body );
     req.end();
-}
-
-
-function ping(appmod, callback) {
-    const options = {
-        host: 'localhost',
-        port: '7181',
-        path: '/erllambda/' + appmod,
-        method: 'GET'
-    };
-    var req = http.request( options, function(response) {
-        if( response.statusCode == 200 ) callback();
-        else callback( {error: 'ping response status: '
-                        + response.statusCode} );
-    }).on('error', function(error) {
-        callback(error);
-    }).end();
-}
-
-
-function alive(appmod, deadline, callback) {
-    const delay = 100;
-    ping( appmod, function(err) {
-        if( !err ) callback();
-        else if( Date.now() < deadline ) {
-            console.log( 'waiting %s ms for erlang start', delay );
-            setTimeout( function() {
-                alive(appmod, deadline, callback)
-            }, delay );
-        }
-        else {
-            callback( { action: 'alive', error: 'timeout waiting for vm' } );
-        }
-    });
-}
-
-
-function start(appmod, script, env, callback) {
-    const tmp = require('tmp').dirSync();
-    const glob = require('glob');
-    const command = script + ' start';
-    /* set some additional environment variable needed to drive rebar
-       release scripts. */
-    env.RELX_REPLACE_OS_VARS = "true";
-    env.CORESRV_RUNDIR = tmp.name;
-    env.CORESRV_PROGNAME = appmod;
-    env.ERTS_DIR = glob.sync( 'erts-*' )[0];
-    env.HOME = tmp.name;
-    /* no matter what we just issue a stop without looking, to be safe */
-    stop( script, function() {
-        console.log( 'executing: "%s" with env: %s', command,
-                     JSON.stringify(env) );
-        exec( command, {env: env}, function(err, stdout, stderr) {
-            if( err )
-                callback(err);
-            else {
-                const deadline = Date.now() + 10000;
-                alive(appmod, deadline, callback);
-            }
-        });
-    });
-}
-
-function stop(script, callback) {
-    const command = script + ' stop';
-    console.log( 'executing: "%s"', command );
-    exec( command, {}, function() {
-        callback();
-    });
-}
+};
