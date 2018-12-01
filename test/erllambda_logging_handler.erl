08da77beff7c950dde1b24f6af98a3aa9fa304b6
Grigory Starinkin <starinkin@gmail.com>
2018-12-01 14:11:38 +0000

    fix spelling in common test

--- test/erllambda_logging_handler.erl
+++ test/erllambda_logging_handler.erl
@@ -9,6 +9,6 @@
 
 handle(_Event, _Context) ->
     erllambda:message("Starting logs test"),
-    erllambda:message("This line should not brake logging", []),
+    erllambda:message("This line should not break logging", []),
     erllambda:message("This and any further logs should be printed"),
     {ok, "done"}.
