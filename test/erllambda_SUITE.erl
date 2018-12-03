6372013b58bc095eef2dc80cf6a1eb12bebde5e7
Grigory Starinkin <starinkin@gmail.com>
2018-12-03 20:09:43 +0000

    add proxy handler to be able to call more than 1 handler per suite

--- test/erllambda_SUITE.erl
+++ test/erllambda_SUITE.erl
@@ -18,19 +18,12 @@ suite() ->
 %% @end
 %%--------------------------------------------------------------------
 init_per_suite(Config) ->
-    {ok, _} = erllambda_aws_runtime:start(),
-    Port = erllambda_aws_runtime:http_port(),
-    RunTimeAddress = lists:flatten(io_lib:format("127.0.0.1:~w", [Port])),
-    OldEnv =
-        putenv([{"AWS_LAMBDA_RUNTIME_API", RunTimeAddress},
-                {"AWS_ACCESS_KEY_ID", "10560ff7be594e0c"},
-                {"AWS_SECRET_ACCESS_KEY", "71b5110832454df0ba9a85073d60bb8b"},
-                {"AWS_SESSION_TOKEN", "c14c5ae0d56242cbb2d85a8cf1433ece"}]),
+    AWSConfig = erllambda_ct:init_aws_env(),
     application:load(erllambda),
-    application:set_env(erllambda, handler_module, erllambda_fibonachi_handler),
+    application:set_env(erllambda, handler_module, erllambda_proxy_handler),
     application:set_env(erllambda, print_env, false),
     {ok, _} = application:ensure_all_started(erllambda),
-    [{old_env, OldEnv} | Config].
+    AWSConfig ++ Config.
 
 %%--------------------------------------------------------------------
 %% @spec end_per_suite(Config0) -> term() | {save_config,Config1}
@@ -39,9 +32,8 @@ init_per_suite(Config) ->
 %%--------------------------------------------------------------------
 end_per_suite(Config) ->
     application:stop(erllambda),
-    putenv(?config(old_env, Config)),
     application:unload(erllambda),
-    ok = erllambda_aws_runtime:stop().
+    erllambda_ct:destruct_aws_env(Config).
 
 %%--------------------------------------------------------------------
 %% @spec init_per_group(GroupName, Config0) ->
@@ -118,14 +110,6 @@ all() ->
 %%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @spec TestCase() -> Info
-%% Info = [tuple()]
-%% @end
-%%--------------------------------------------------------------------
-test_fibonachi() ->
-    [].
-
-%%--------------------------------------------------------------------
 %% @spec TestCase(Config0) ->
 %%               ok | exit() | {skip,Reason} | {comment,Comment} |
 %%               {save_config,Config1} | {skip_and_save,Reason,Config1}
@@ -135,6 +119,7 @@ test_fibonachi() ->
 %% @end
 %%--------------------------------------------------------------------
 test_fibonachi(_Config) ->
+    erllambda_proxy_handler:delegate_to(erllambda_fibonachi_handler),
     {ok, #{<<"sequence">> := [0, 1, 1, 2, 3, 5]}} =
         lists:foldl(
           fun(_, {ok, Params}) -> erllambda_aws_runtime:call(Params) end,
@@ -145,15 +130,3 @@ test_fibonachi(_Config) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
-putenv([{Key, Value} | Tail]) ->
-    OldValue = os:getenv(Key, undefined),
-    putenv(Key, Value),
-    [{Key, OldValue} | putenv(Tail)];
-putenv([]) ->
-    [].
-
-putenv(Key, undefined) ->
-    os:unsetenv(Key);
-putenv(Key, Value) ->
-    os:putenv(Key, Value).
